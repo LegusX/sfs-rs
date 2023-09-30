83dc649a7a13226b045977fc15f142c88453c119
Logan Henrie <loghenrie@gmail.com>
2023-09-29 22:29:32 -0400

    Fixed run errors on windows

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,11 @@
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use std::str;
+use fancy_regex::Regex;
 use std::env;
 use std::path::Path;
 use simple_home_dir::home_dir;
-use std::process::Command;
-use std::str;
-use fancy_regex::Regex;
 
 #[derive(Debug)]
 #[derive(Hash)]
@@ -22,6 +21,11 @@ pub struct Config {
     path: String,
     pub vdf: Vec<Device>,
     raw: String,
+}
+
+pub enum Error {
+    NoAuthorizedDevice,
+    ConfigNotFound,
 }
 impl Config {
     //Writes the new AuthorizedDevice value to the config file
@@ -48,61 +52,24 @@ impl Config {
 
         fs::write(&self.path, new_text.as_ref()).expect("Failed to write to config file!");
     }
+    pub fn init(&mut self, config_path: String) -> Result<(), Error> {
+        let raw_vdf = read_steam_config(&config_path);
+        create_backup(&raw_vdf, &config_path);
+        let vdf = parse_raw_vdf(&raw_vdf)?;
+        self.path = config_path;
+        self.vdf = vdf;
+        self.raw = raw_vdf.to_string();
+        Ok(())
+    }
 }
 
 pub fn new() -> Config {
-    let config_path = get_config_path();
-    let raw_vdf = read_steam_config(&config_path);
-    create_backup(&raw_vdf, &config_path);
-    let vdf = parse_raw_vdf(&raw_vdf);
-
     let config = Config {
-        path: config_path,
-        vdf,
-        raw: raw_vdf.to_string(),
+        path: Default::default(),
+        vdf: Default::default(),
+        raw: Default::default(),
     };
     return config;
-}
-
-fn get_config_path() -> String {
-    let vdf_path: String;
-    if cfg!(debug_assertions) {
-        vdf_path = String::from("./config.vdf");
-    } else if cfg!(target_os = "linux") {
-        let linux_path = format!(
-            "{}/.local/share/Steam/config/config.vdf",
-            home_dir().unwrap().to_string_lossy()
-        );
-        if file_exists(linux_path.as_ref()) {
-            vdf_path = linux_path;
-        } else {
-            panic!("Can't find config path");
-        }
-    } else if cfg!(target_os = "windows") {
-        let output = Command::new("where")
-            .arg("steam")
-            .output()
-            .expect("Failed to execute process");
-        let steam_install_str = str
-            ::from_utf8(output.stdout.as_slice())
-            .expect("Failed to retrieve steam install path");
-        let steam_install_path = Path::new(steam_install_str);
-        let steam_root: &str = steam_install_path.parent().unwrap().to_str().unwrap();
-        let windows_path = format!("{}/config/config.vdf", steam_root);
-
-        if file_exists(&windows_path) {
-            vdf_path = format!("{}/config/config.vdf", steam_root);
-        } else {
-            panic!("Can't find config path");
-        }
-    } else {
-        panic!("OS {} not supported", env::consts::OS);
-    }
-    return vdf_path;
-}
-
-fn file_exists(path: &str) -> bool {
-    return Path::new(path).is_file();
 }
 
 fn read_steam_config(path: &str) -> String {
@@ -119,7 +86,7 @@ fn create_backup(raw: &str, config_path: &str) -> () {
 }
 
 //Creates a struct to represent the config.vdf
-fn parse_raw_vdf(file: &str) -> Vec<Device> {
+fn parse_raw_vdf(file: &str) -> Result<Vec<Device>, Error> {
     let mut data: Vec<Device> = Default::default();
     let regex: Regex = Regex::new(
         r#"(?P<id>\d+)"\s*\{\s*"timeused"\s*"(?P<timeused>\d+)"\s*"description"\s*"(?P<description>[^"]+)"\s*"tokenid"\s*"(?P<tokenid>-?\d+)"\s*}"#
@@ -127,7 +94,12 @@ fn parse_raw_vdf(file: &str) -> Vec<Device> {
     let captures = regex.captures_iter(file);
 
     for capture in captures {
-        let unwrapped = capture.unwrap();
+        let unwrapped = match capture {
+            Ok(c) => c,
+            Err(_e) => {
+                return Err(Error::NoAuthorizedDevice);
+            }
+        };
         let device = Device {
             id: unwrapped.get(1).unwrap().as_str().to_string(),
             timeused: unwrapped.get(2).unwrap().as_str().to_string(),
@@ -136,5 +108,36 @@ fn parse_raw_vdf(file: &str) -> Vec<Device> {
         };
         data.push(device);
     }
-    return data;
+    return Ok(data);
+}
+
+pub fn get_config_path() -> Result<String, Error> {
+    let vdf_path: String;
+    if cfg!(debug_assertions) && !cfg!(target_os = "windows") {
+        vdf_path = String::from("./config.vdf");
+    } else if cfg!(target_os = "linux") {
+        let linux_path = format!(
+            "{}/.local/share/Steam/config/config.vdf",
+            home_dir().unwrap().to_string_lossy()
+        );
+        if file_exists(linux_path.as_ref()) {
+            vdf_path = linux_path;
+        } else {
+            return Err(Error::ConfigNotFound);
+        }
+    } else if cfg!(target_os = "windows") {
+        let windows_path = r#"C:\Program Files (x86)\Steam\config\config.vdf"#;
+        if file_exists(windows_path) {
+            vdf_path = windows_path.to_string();
+        } else {
+            return Err(Error::ConfigNotFound);
+        }
+    } else {
+        panic!("OS {} not supported", env::consts::OS);
+    }
+    return Ok(vdf_path);
+}
+
+fn file_exists(path: &str) -> bool {
+    return Path::new(path).is_file();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
+#![windows_subsystem = "windows"]
+
 use eframe::egui;
 use egui::{ CentralPanel, TopBottomPanel, Window };
 use egui_dnd::dnd;
+use rfd::FileDialog;
 
 mod config;
 
 fn main() {
     let mut config = config::new();
     let mut steam_closed = false;
+    let mut steam_config_found = false;
+
+    //If the config path can be found, initiate the config immediately, if not, request for user to direct to it
+    let config_path = match config::get_config_path() {
+        Ok(r) => r,
+        Err(_e) => String::from(""),
+    };
 
     eframe
         ::run_simple_native("sfs-rs", Default::default(), move |ctx, _frame| {
@@ -30,9 +40,63 @@ fn main() {
                             steam_closed = true;
                         }
                     });
+            } else if config_path.len() > 0 {
+                match config.init(config_path.clone()) {
+                    Err(e) => {
+                        match e {
+                            config::Error::NoAuthorizedDevice => {
+                                Window::new("Error:")
+                                    .anchor(egui::Align2::CENTER_CENTER, [0.0, 0.0])
+                                    .resizable(false)
+                                    .collapsible(false)
+                                    .show(ctx, |ui| {
+                                        ui.label(
+                                            "Failed to find any accounts connected to Family Sharing."
+                                        )
+                                    });
+                            }
+                            config::Error::ConfigNotFound => {} //Handled below
+                        }
+                    }
+                    _ => {
+                        steam_config_found = true;
+                    }
+                }
+            } else if !steam_config_found {
+                Window::new("Can't find Steam config file")
+                    .anchor(egui::Align2::CENTER_CENTER, [0.0, 0.0])
+                    .resizable(false)
+                    .collapsible(false)
+                    .show(ctx, |ui| {
+                        ui.label(
+                            "Steam config file could not be found. Please choose the file manually"
+                        );
+                        if ui.button("Choose file").clicked() {
+                            if
+                                let Some(path) = FileDialog::new()
+                                    .add_filter("config", &["vdf"])
+                                    .pick_file()
+                            {
+                                steam_config_found = true;
+                                match config.init(path.to_string_lossy().to_string()) {
+                                    Err(e) => {
+                                        match e {
+                                            _ => {
+                                                Window::new("Hello world").show(ctx, |ui| {
+                                                    ui.label("Hi");
+                                                });
+                                            }
+                                        }
+                                    }
+                                    _ => {}
+                                };
+                            }
+                        }
+                    });
             }
             CentralPanel::default().show(ctx, |ui| {
                 //Drag and drop handler
+                // println!("{:?}", &config.vdf);
                 dnd(ui, "reorder_dnd").show_vec(&mut config.vdf, |ui, item, handle, _state| {
                     ui.horizontal(|ui| {
                         handle.ui(ui, |ui| {
@@ -40,8 +104,10 @@ fn main() {
                         });
                     });
                 });
+
                 if ui.button("Save config").clicked() {
-                    config.write();
+                    // config.write();
+                    println!("{:?}", config.vdf.pop());
                 }
             });
         })
